2a7d70c7f49af2813a22aa62b3b6d26d247b6e6b
Michael <michael@thegur.net>
2017-07-18 21:52:39 +0300

    added basic gui

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import gui.Table;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
 
 /**
  * Created by Michael on 7/18/2017.
  */
 public class Main
 {
-    static byte[] readBinaryFile(String filename) {
-        try { return Files.readAllBytes(Paths.get(filename)); }
-        catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error: Could Not Read " + filename);
-        }
-    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(700, 500);
+        frame.setTitle("KLMN Hex Editor");
+        frame.setLocationRelativeTo(null);
 
-    static void writeBinaryFile(byte[] bytes, String filename) {
-        try { Files.write(Paths.get(filename), bytes); }
-        catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error: Could Not Write to " + filename);
-        }
-    }
+        String[] columns = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+        Object[][] data = {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+                {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
+        };
+        Table table = new Table(data, columns);
+        table.setBorder(null);
 
-    public static void main(String[] args) {}
+        frame.getContentPane().add(table);
+        frame.setVisible(true);
+    }
 }
