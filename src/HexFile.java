a14c5777b7b60db64e263c4dea0817ba44aca98b
Yonatan Schachter <yonatan.schachter@gmail.com>
2017-07-19 13:16:14 +0300

    fixed writing

--- src/HexFile.java
+++ src/HexFile.java
@@ -11,12 +11,14 @@ public class HexFile {
     private String path;
     private String fileName;
     private Byte[][] dataArray;
+    private int size;
 
 
     HexFile(){}
     HexFile(String path){
 
         byte[] byteArray = readBinaryFile(path);
+        size = byteArray.length;
         dataArray = new Byte[(int)Math.ceil(byteArray.length/16.0)][16];
 
 
@@ -44,9 +46,11 @@ public class HexFile {
     }
 
     public void saveAs(String p){
-        byte[] b = new byte[dataArray.length];
-        for (int i = 0; i < dataArray.length; i++)
-            b[i] = dataArray[i/16][i%16];
+        byte[] b = new byte[size];
+
+        for (int i = 0; i < size; i++) {
+            b[i] = dataArray[i / 16][i % 16];
+        }
         writeBinaryFile(b, p);
     }
     public void save(){
